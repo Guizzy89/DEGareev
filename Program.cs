a2e399d7e4afcf107ff632f02fbca674ad838be2
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-03-01 22:01:20 +0300

    убрал исполнителя из конструктора заказов. так же изменил автоматическое изменени статуса из WaitingForExecution на InRepair при назначении исполнителя.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-Order order1 = new (1, " ", " ", " ", " ", " " ); 
-
+Order order1 = new (1, " ", " ", " ", " " );
+List<string> executors = ["Ivan", "Petr", "Sergey"];
 app.MapGet("/", () => "Hello World!");
 app.MapGet("orders", () => order1);
 
@@ -10,19 +10,34 @@ app.Run();
 
 public class Order
 {
-    public Order(int OrderNumber, string Device, string ProblemType, string ClientName, string ClientSurname, string Executor)
+    public Order(int orderNumber, string device, string problemType, string clientName, string clientSurname)
     {
+        this.OrderNumber = orderNumber;
+        this.Device = device;
+        this.ProblemType = problemType;
+        this.ClientName = clientName;
+        this.ClientSurname = clientSurname;
     }
 
     public int OrderNumber { get; set; }
     public DateOnly Orderdate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     public string Device { get; set; }
     public string ProblemType { get; set; }
-    public string? Description { get; set; }
     public OrderStatus Status { get; set; } = OrderStatus.WaitingForExecution;
     public string ClientName { get; set; }
-    public string ClientSurname { get; set; }
-    public string? Executor { get; set; }
+    public string ClientSurname { get; set; }    
+    public string? Description { get; set; }
+    private string? _executor;
+    public string? Executor
+    {
+        get => _executor;
+        set
+        {
+            _executor = value;
+            if (_executor != null)
+                Status = OrderStatus.InRepair;
+        }
+    }
 }
 
 public enum OrderStatus
