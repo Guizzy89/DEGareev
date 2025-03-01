e0592777aa539f5581b048808f5652263e4bb7f6
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-03-01 15:18:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+Order order1 = new (1, " ", " ", " ", " ", " ", " " ); 
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("orders", () => order1);
+
+app.Run();
+
+public class Order
+{
+    public Order(int OrderNumber, string Device, string ProblemType, string Description, string ClientName, string ClientSurname, string Executor)
+    {
+    }
+
+    public int OrderNumber { get; set; }
+    public DateTime Orderdate { get; set; } = DateTime.Now;
+    public string Device { get; set; }
+    public string ProblemType { get; set; }
+    public string Description { get; set; }
+    public OrderStatus Status { get; set; } = OrderStatus.WaitingForExecution;
+    public string ClientName { get; set; }
+    public string ClientSurname { get; set; }
+    public string? Executor { get; set; }
+}
+
+public enum OrderStatus
+{
+    WaitingForExecution,
+    InRepair,
+    ReadyToIssue
+}
